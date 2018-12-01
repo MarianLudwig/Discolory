cb6bfe964163f4171959c71461d1b964727bcb87
Pierre <Pierry@web.de>
2018-12-01 05:10:55 +0100

    Mirror Game setup

---
+++ Discolory/Assets/Scripts/MirrorPlane.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorPlane : MonoBehaviour {
+
+    public GameObject child;
+    private LineRenderer lr;
+
+    private bool updateBeam = false;
+
+	// Use this for initialization
+	void Start () {
+        lr = child.GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(updateBeam == true)
+        {
+            lr.enabled = false;
+        }
+        if (lr.enabled)
+        {
+            RaycastHit hit;
+
+            lr.SetPosition(0, this.transform.position);
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.collider)
+                {
+                    lr.SetPosition(1, hit.point);
+                }
+            }
+            else lr.SetPosition(1, transform.forward * 5000);
+            updateBeam = true;
+        }
+        
+	}
+
+    public void activateLaser()
+    {
+        if (!lr.enabled)
+        {
+            lr.enabled = true;
+            updateBeam = false;
+        }
+    }
+
+    public void rotateRight()
+    {
+        var angles = transform.rotation.eulerAngles;
+        Debug.Log("rotate Right: " + angles.y);
+        if (angles.y < 45+180 || angles.y > 295-180)
+        {
+            angles.y += 15;
+            transform.rotation = Quaternion.Euler(angles);
+        }
+
+    }
+
+    public void rotateLeft()
+    {
+        var angles = transform.rotation.eulerAngles;
+        Debug.Log("rotate Left: " + angles.y);
+        if (angles.y > 310-180 || angles.y < 50+180)
+        {
+            angles.y -= 15;
+            transform.rotation = Quaternion.Euler(angles);
+        }
+    }
+}
