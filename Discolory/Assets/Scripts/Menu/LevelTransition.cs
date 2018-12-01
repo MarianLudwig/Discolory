303e72bee4abbb01dd3f2f630341caabad4a0930
Jeff-Owens Iyalekhue <j.oiyalekhue@hotmail.de>
2018-12-01 07:46:28 +0100

    transition fade added

---
+++ Discolory/Assets/Scripts/Menu/LevelTransition.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class LevelTransition : MonoBehaviour {
+
+    public static LevelTransition Instance;
+    private Animator animator;
+    private int sceneToLoad = 1;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        animator = GetComponent<Animator>();
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            FadeToScene();
+        }
+	}
+
+    public void FadeToScene()
+    {
+        animator.SetTrigger("In");
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            sceneToLoad = 1;
+        }
+        else
+        {
+            sceneToLoad = 0;
+        }
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
--- Discolory/Assets/Scripts/Menu/StartGame.cs
+++ Discolory/Assets/Scripts/Menu/StartGame.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour {
 
@@ -11,6 +10,6 @@ public class StartGame : MonoBehaviour {
         GameData.Instance.SaveSettings();
 
         // Load Scene with the Loading ID 1
-        SceneManager.LoadScene(1);
+        LevelTransition.Instance.FadeToScene();
     }
 }
