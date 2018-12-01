39a43011a10c87064947cb4ce1716542a05c0e71
Marian Ludwig <ga86lop@mytum.de>
2018-12-01 16:50:17 +0100

    Lightbeam

--- Discolory/Assets/Scripts/Player/PlayerMovement.cs
+++ Discolory/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,16 +7,22 @@ using System;
 [Serializable]
 public class ControllerSettings
 {
-	public string leftHorizontal;
-	public string leftVertical;
+	public string leftHorizontal = "leftHorizontal_P1";
+	public string leftVertical = "leftVertical_P1";
 	[Tooltip("Right Stick x-Axis (4th axis")]
-	public string rightHorizontal;
+	public string rightHorizontal = "rightHorizontal_P1";
 	[Tooltip("Right Stick y-Axis (5th axis)")]
-	public string rightVertical;
-	public string interactionButton;
-	public string changeButton;
+	public string rightVertical = "rightVertical_P1";
+	public string interactionButton = "Interaction_P1";
+	public string changeButton = "Change_P1";
+	public string rightTrigger = "rightTrigger_P1";
 }
 
+public enum PrimaryColors
+{
+	Red,
+	Blue,
+}
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField]
@@ -26,20 +32,45 @@ public class PlayerMovement : MonoBehaviour
 	public float bodyRotSpeed;
 	public float headRotSpeed;
 
+	// for managing lightbeam stuff
+	public GameObject staff;
+
 	private float minAngle = 45f;
 	private float maxAngle = -45f;
 
 	private float cameraAngle;
 
+	[SerializeField]
+	public PrimaryColors primaryColor;
+	private Color primColor;
+	private bool primaryColorActive; //primary color or yellow active?
+
+	private bool changingGem;
+
+	private Vector3 spawnPos;
 	// Use this for initialization
 	void Start()
 	{
-		
+		spawnPos = transform.position;
+		//start with primaryColor
+		primaryColorActive = true;  
+		if (primaryColor == PrimaryColors.Red)
+			primColor = Color.red;
+		else
+			primColor = Color.blue;
+		staff.GetComponent<StaffBehaviour>().ChangeGem(primColor);
+	}
+
+	void Reset()
+	{
+		transform.position = spawnPos;
+		changingGem = false;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		#region INPUT
 		if (Input.GetAxis(conSettings.leftHorizontal) != 0)
 		{
 			transform.Translate(Input.GetAxis(conSettings.leftHorizontal) * walkingSpeed * Time.deltaTime, 0, 0);
@@ -61,9 +92,36 @@ public class PlayerMovement : MonoBehaviour
 		{
 
 		}
+
+		// Return if animation currently played
+		if (changingGem)
+			return;
+
 		if (Input.GetButtonDown(conSettings.changeButton))
 		{
+			Debug.Log("Changing gem");
+			//changingGem = true;
+			if (primaryColorActive)
+			{
+				staff.GetComponent<StaffBehaviour>().ChangeGem(Color.yellow);
+			}
+			else
+			{
+				staff.GetComponent<StaffBehaviour>().ChangeGem(primColor);
+			}
 
+			primaryColorActive = !primaryColorActive;
 		}
+		if (Input.GetButtonDown(conSettings.rightTrigger))
+		{
+			// Activate light beam		
+			staff.GetComponent<StaffBehaviour>().ActivateLightBeam();
+		}
+		if (Input.GetButtonUp(conSettings.rightTrigger))
+		{
+			// Activate light beam		
+			staff.GetComponent<StaffBehaviour>().DeactivateLightBeam();
+		}
+		#endregion
 	}
 }
---
+++ Discolory/Assets/StaffBehaviour.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffBehaviour : MonoBehaviour
+{
+
+	public float energyConsumption = 3f;
+
+	private bool lightBeamActive;
+	private Transform muzzle;
+	private LineRenderer lightBeam;
+	// Layer 9-14; R - Y(G) - B - O - P - G, only RYB usable for lightbeam, purple objects can be hit by both
+	private int validLayermask;
+
+	// How long/much can the lightBeam be used, decreases in Update while lightBeamActive
+	private float lightPower = 100f;
+
+	// Use this for initialization
+	void Start()
+	{
+		muzzle = transform.GetChild(0);
+		lightBeam = muzzle.GetComponent<LineRenderer>();
+		lightBeam.enabled = false;
+		validLayermask = LayerMask.GetMask("default"); // Yellow, standard
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (lightBeamActive)
+		{
+			if (lightPower <= 0)
+			{
+				lightPower = 0;
+				DeactivateLightBeam();
+				return;
+			}
+
+			lightBeam.SetPosition(0, muzzle.transform.position);
+			RaycastHit hit;
+			if (Physics.Raycast(muzzle.transform.position, muzzle.transform.forward, out hit, 100f, validLayermask))
+			{
+				Debug.Log("hit");
+				if (hit.collider)
+				{
+					lightBeam.SetPosition(1, hit.point);
+					// TODO Call function on hit object if it is lightbeam-interactable
+				}
+			}
+			else
+			{
+				lightBeam.SetPosition(1, muzzle.transform.position + muzzle.transform.forward * 1000f);
+				Debug.Log("no hit");
+			}
+
+			lightPower -= Time.deltaTime * energyConsumption;
+		}
+	}
+
+	public void ActivateLightBeam()
+	{
+		if (lightPower <= 0)
+			return;
+		lightBeamActive = true;
+		lightBeam.enabled = true;
+	}
+
+	public void DeactivateLightBeam()
+	{
+		lightBeamActive = false;
+		lightBeam.enabled = false;
+	}
+
+	public void ChangeGem(Color changeToColor)
+	{
+		lightBeam.startColor = changeToColor;
+
+		if (changeToColor == Color.red)
+			validLayermask = LayerMask.GetMask("Default", "Red");
+		else if (changeToColor == Color.yellow)
+			validLayermask = LayerMask.GetMask("Default", "Yellow");
+		else									
+			validLayermask = LayerMask.GetMask("Default", "Blue");
+	}
+}
